4c1a00274778ff7fd6a64541d12211ab7bd28718
dioph <dioph@protonmail.com>
2022-05-18 09:08:13 -0300

    updated ceemdan/spline tests

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,7 +1,7 @@
 import pytest
 import torch
 
-from sigzyme.core import TorchEMD
+from sigzyme.core import TorchEMD, TorchCEEMDAN
 
 
 def test_emd_two_sinusoids_plus_trend():
@@ -41,11 +41,38 @@ def test_emd_max_modes():
 def test_emd_gpu():
     if not torch.cuda.is_available():
         pytest.skip("skipping CUDA tests")
-    t = torch.linspace(0, 10, 1024).to(float)
+    torch.manual_seed(42)
+    t = torch.linspace(0, 10, 64).to(float)
     t0 = t.to("cuda")
     X = torch.atleast_2d(torch.randn(t.shape)).to(float)
     X0 = X.to("cuda")
     emd = TorchEMD()
-    cpu_modes = torch.vstack(emd(t, X))
-    gpu_modes = torch.vstack(emd(t0, X0))
-    assert torch.allclose(gpu_modes.cpu(), cpu_modes)
+    cpu_modes = emd(t, X)
+    gpu_modes = emd(t0, X0)
+    assert torch.allclose(gpu_modes.cpu(), cpu_modes, atol=1e-6)
+
+
+def test_ceemdan_two_tones():
+    # Test if nothing but the two tones are recovered by CEEMDAN
+    t = torch.arange(1000).to(float)
+    s2 = torch.sin(2 * torch.pi * 0.065 * t)
+    s1 = torch.zeros_like(s2)
+    s1[500:750] += torch.sin(2 * torch.pi * 0.255 * torch.arange(250))
+    data = s1 + s2
+    imfs = TorchCEEMDAN(ensemble_size=50, random_seed=42)(t, data)
+    assert len(imfs) == 2
+    # Test if the residual noise in the first mode is close to zero
+    left_mse = torch.mean(torch.square(imfs[0][0][11:490]))
+    right_mse = torch.mean(torch.square(imfs[0][0][761:990]))
+    assert left_mse < 1e-4
+    assert right_mse < 1e-4
+    # Test the closeness between the original tones and the recovered IMFs
+    err1 = (imfs[0] - s1)[:, 3:-3]
+    err2 = (imfs[1] - s2)[:, 3:-3]
+    err = sum(imfs) - data
+    rrse_1 = torch.linalg.norm(err1) / torch.linalg.norm(s1[3:-3])
+    rrse_2 = torch.linalg.norm(err2) / torch.linalg.norm(s2[3:-3])
+    rrse_x = torch.linalg.norm(err) / torch.linalg.norm(data)
+    assert rrse_1 < 0.10
+    assert rrse_2 < 0.05
+    assert rrse_x < 1e-16
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,32 +1,139 @@
 import numpy as np
+import pytest
 from scipy import interpolate
 import torch
 
-from sigzyme.utils import torch_akima, torch_cubic, torch_hermite
+from sigzyme.utils import (
+    torch_akima,
+    torch_natural,
+    torch_not_a_knot,
+    torch_hermite,
+    torch_peaks,
+)
 
 
-def test_akima_scipy():
-    t = torch.arange(5).to(float)
-    y = torch.tensor([3, 2, 3, 4, 3]).to(float)
+@pytest.fixture
+def t():
+    return torch.arange(5).to(float)
+
+
+@pytest.fixture
+def y():
+    return torch.tensor([3, 2, 3, 4, 3]).to(float)
+
+
+@pytest.fixture
+def x_rep():
+    x1 = torch.tensor([-1.5, -0.5, 0.5, 1.5, 2.5, 3.5, 4.5, 5.5, 6.5])
+    x2 = torch.tensor([-2, -1, 1, 2, 3, 3, 3, 7, 8])
+    x3 = torch.tensor([-2, -2, 2, 2, 2, 2, 2, 8, 8])
+    return torch.stack([x1, x2, x3]).to(float)
+
+
+@pytest.fixture
+def x_norep():
+    x1 = torch.tensor([-1.5, -0.5, 0.5, 1.5, 2.5, 3.5, 4.5, 5.5, 6.5])
+    x2 = torch.tensor([-2, -1, 1, 2, 3, 7, 8])
+    x3 = torch.tensor([-2, 2, 8])
+    return [x1, x2, x3]
+
+
+@pytest.fixture
+def y_rep():
+    y1 = torch.tensor([2, 3, 3, 2, 3, 4, 3, 3, 4])
+    y2 = torch.tensor([3, 2, 2, 3, 4, 4, 4, 4, 3])
+    y3 = torch.tensor([1, 1, 1, 1, 1, 1, 1, 1, 1])
+    return torch.stack([y1, y2, y3]).to(float)
+
+
+@pytest.fixture
+def y_norep():
+    y1 = torch.tensor([2, 3, 3, 2, 3, 4, 3, 3, 4])
+    y2 = torch.tensor([3, 2, 2, 3, 4, 4, 3])
+    y3 = torch.tensor([1, 1, 1])
+    return [y1, y2, y3]
+
+
+def test_akima_scipy(t, y):
     t0 = torch.arange(0, 4.5, 0.5).to(float)
     y0 = torch_akima(t, y, t0)
     true_y0 = interpolate.Akima1DInterpolator(t, y)(t0)
     assert np.allclose(y0, true_y0)
 
 
-def test_hermite_scipy():
-    t = torch.arange(5).to(float)
-    y = torch.tensor([3, 2, 3, 4, 3]).to(float)
+def test_hermite_scipy(t, y):
     t0 = torch.arange(0, 4.5, 0.5).to(float)
     y0 = torch_hermite(t, y, t0)
     true_y0 = interpolate.CubicHermiteSpline(t, y, dydx=np.gradient(y, t))(t0)
     assert np.allclose(y0, true_y0)
 
 
-def test_cubic_scipy():
-    t = torch.arange(5).to(float)
-    y = torch.tensor([3, 2, 3, 4, 3]).to(float)
+def test_natural_scipy(t, y):
     t0 = torch.arange(0, 4.5, 0.5).to(float)
-    y0 = torch_cubic(t, y, t0)
+    y0 = torch_natural(t, y, t0)
     true_y0 = interpolate.CubicSpline(t, y, bc_type="natural")(t0)
     assert np.allclose(y0, true_y0)
+
+
+def test_nak_scipy(t, y):
+    t0 = torch.arange(0, 4.5, 0.5).to(float)
+    y0 = torch_not_a_knot(t, y, t0)
+    true_y0 = interpolate.CubicSpline(t, y, bc_type="not-a-knot")(t0)
+    assert np.allclose(y0, true_y0)
+
+
+def test_akima_with_repetition(x_rep, y_rep, x_norep, y_norep):
+    t0 = torch.arange(0, 5.5, 0.5).expand(3, -1).contiguous()
+    y0 = torch_akima(x_rep, y_rep, t0)
+    for i in range(3):
+        true_y0 = interpolate.Akima1DInterpolator(x_norep[i], y_norep[i])(t0[i])
+        assert np.allclose(y0[i], true_y0)
+
+
+def test_hermite_with_repetition(x_rep, y_rep, x_norep, y_norep):
+    t0 = torch.arange(0, 5.5, 0.5).expand(3, -1).contiguous()
+    y0 = torch_hermite(x_rep, y_rep, t0)
+    for i in range(3):
+        true_y0 = interpolate.CubicHermiteSpline(
+            x_norep[i], y_norep[i], dydx=np.gradient(y_norep[i], x_norep[i])
+        )(t0[i])
+        assert np.allclose(y0[i], true_y0)
+
+
+def test_natural_with_repetition(x_rep, y_rep, x_norep, y_norep):
+    t0 = torch.arange(0, 5.5, 0.5).expand(3, -1).contiguous()
+    y0 = torch_natural(x_rep, y_rep, t0)
+    for i in range(3):
+        true_y0 = interpolate.CubicSpline(x_norep[i], y_norep[i], bc_type="natural")(
+            t0[i]
+        )
+        assert np.allclose(y0[i], true_y0)
+
+
+def test_nak_with_repetition(x_rep, y_rep, x_norep, y_norep):
+    t0 = torch.arange(0, 5.5, 0.5).expand(3, -1).contiguous()
+    y0 = torch_not_a_knot(x_rep, y_rep, t0)
+    for i in range(3):
+        true_y0 = interpolate.CubicSpline(x_norep[i], y_norep[i], bc_type="not-a-knot")(
+            t0[i]
+        )
+        assert np.allclose(y0[i], true_y0)
+
+
+def test_peaks_constant():
+    batch, cols = torch_peaks(torch.ones(10), method="ffill")
+    assert len(batch) == 0
+    assert len(cols) == 0
+
+
+def test_peaks_plateaus():
+    plateau_sizes = torch.tensor([1, 2, 3, 4, 8, 20, 111])
+    x = torch.zeros(len(plateau_sizes) * 2 + 1)
+    x[1::2] = plateau_sizes
+    repeats = torch.ones(x.shape, dtype=int)
+    repeats[1::2] = x[1::2]
+    x = torch.repeat_interleave(x, repeats)
+    batch, cols = torch_peaks(x, method="ffill")
+    assert torch.allclose(cols, torch.tensor([1, 3, 6, 10, 15, 24, 45]))
+    batch, cols = torch_peaks(x, method="bfill")
+    assert torch.allclose(cols, torch.tensor([1, 4, 8, 13, 22, 43, 155]))
